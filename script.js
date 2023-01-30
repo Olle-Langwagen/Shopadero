0c5ab4acb80dacfb50e5ad8d523771f44496578f
Olle-Langwagen <olle.langwagen@elev.ga.ntig.se>
2023-01-30 22:28:17 +0100

    contactMessege, fixes, news work

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
+
+/* Newsletter subscribe info */
+document.querySelector("#news-button").onclick = function() {
+    const h4 = document.querySelector("#news-h4");
+    h4.innerHTML = "Thank you for subscribing!";
+    h4.style.color = "green";
+    const p = document.querySelector(".news-p");
+    p.innerHTML = "You will receive our newsletters every month.";
+    const input = document.querySelector("#news-input");
+    input.style.display = "none";
+    const button = document.querySelector("#news-button");
+    button.style.display = "none";
+
+}
+
+
 if(bar){
     bar.addEventListener('click', () => {
         nav.classList.add('active');
